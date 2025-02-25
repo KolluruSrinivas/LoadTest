72948a3cb742384ecade9a951074663c8c7327a4
KolluruSrinivas <srinivasraokolluru@gmail.com>
2025-02-25 08:53:12 -0500

    changes

--- CatalogServices/Program.cs
+++ CatalogServices/Program.cs
@@ -1,6 +1,7 @@
 using CatalogService.databases;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
 using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,14 +19,26 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(strConn);
 });
 
+builder.Services.AddSwaggerGen(c =>
+{
+
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatlogService", Version = "v1" });
+
+});
+
+
 var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())*/
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CatalogService v1"));
+
 }
 
 app.UseHttpsRedirection();
